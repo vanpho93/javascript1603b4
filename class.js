6d1db3cfd691c522f296b65fde5f42445691ea06
Admin <vanpho02@gmail.com>
2017-03-23 19:57:32 +0700

    class defind

--- class.js
+++ class.js
@@ -4,14 +4,33 @@
 //getAge
 //setAge
 
-function Person(name, age) {
-    this.name = name;
-    this.getAge = () => age;
-    this.setAge = newAge => { age = newAge; };
+// function Person(name, age) {
+//     this.name = name;
+//     this.age = age;
+//     this.getAge = () => this.age;
+//     this.setAge = newAge => { 
+//         console.log(this);
+//         this.age = newAge; 
+//     };
+// }
+
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+    show() {
+        console.log(this);
+    }
 }
 
+// console.log(typeof Person);
 const pho = new Person('Pho', 18);
-console.log(JSON.stringify(pho));
-console.log(pho.getAge());
-pho.setAge(19);
-console.log(pho.getAge());
+// console.log(pho);
+pho.show();
+
+// const pho = new Person('Pho', 18);
+    // console.log(JSON.stringify(pho));
+    // console.log(pho.getAge());
+    // pho.setAge(19);
+    // console.log(pho.getAge());
